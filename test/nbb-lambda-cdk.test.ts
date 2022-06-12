fe621ff390c9f92d9deccffecf00f8fd1f20e2c5
Jacob Parsell <jakep36@me.com>
2022-06-12 08:28:22 -0600

    Initial commit

---
+++ test/nbb-lambda-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NbbLambdaCdk from '../lib/nbb-lambda-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nbb-lambda-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NbbLambdaCdk.NbbLambdaCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
